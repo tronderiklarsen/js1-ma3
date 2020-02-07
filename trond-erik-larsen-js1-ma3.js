234fedff2ac005dcae2d4338338b209a4d98918c
Trond <56018387+tronderiklarsen@users.noreply.github.com>
2020-02-07 16:19:43 +0100

    Update trond-erik-larsen-js1-ma3.js

--- trond-erik-larsen-js1-ma3.js
+++ trond-erik-larsen-js1-ma3.js
@@ -4,20 +4,79 @@
 
 //2
 
-const baseUrl = "https://api.rawg.io/api/";
-const gamesUrl = `${baseUrl}games?genres=sports`;
-
-fetch(gamesUrl)
+/*
+fetch("https://api.rawg.io/api/games?genres=sports")
     .then(function(response) {
         return response.json();
     })
     .then(function(json) {
-        loadGames(json);
+        viewResults(json);
     })
-    .catch(function(error) {
-        console.log("error.html")
+    .catch(function() {
+        document.location.href = "/error.html"
     });
 
-    function loadGames(json) {
-        console.log(json.results.name);
+    function viewResults(json) {
+        console.dir(json.results);
+        const viewNames = json.results;
+
+        for (let i = 0; i < viewNames; i++) {
+            console.log(json.results);
+        }
     }
+
+// This doesn't work and I give up
+
+*/
+
+//3
+
+/*
+const pSelect = document.querySelector("p");
+
+pSelect.innerText = "These cats are outrageous.";
+
+const newWord = pSelect.innerText.replace("cats", "giraffes");
+
+pSelect.innerText = newWord;
+*/
+
+//4
+
+// I have no idea have to solve this with the curriculum in lesson 4...
+
+//5
+
+/*
+const container = document.querySelector(".container")
+const button = document.querySelector(".btn");
+
+container.removeChild(button);
+*/
+
+//6
+
+// I have no idea have to solve this with what we "learn" in lesson 4...
+
+//7
+
+/*
+Too complicated to solve with what "we have learned". 
+The last couple of weeks have been more confusing than
+anything else. It was fine until Module 1 - Lesson 3.
+Everything after that has been just copying code that
+already has been written by somebody else, without thinking
+for ourselves. The learning curve has been too steep, and 
+every lesson involving RAWG API has too much information 
+per lesson. New stuff getting introduced too quick, with little
+explaination. I get nothing out of it, except getting more 
+and more demotivated. If I can't solve the module assignments 
+from reading and doing lesson assignments, and without getting 
+help every single step, then what's the point? I think alot of
+people in our class will fall off after Module 2 and 3 of
+Javascript 1. I'm starting to think this isn't for me... 
+I doubt I'll even do the second year.
+ 
+    - Trond
+
+*/
